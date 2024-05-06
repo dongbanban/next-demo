c5ae27c24bf1fe7371b62e7031e2da6b9b8606c4
dongbanban <215387880@qq.com>
2024-05-06 11:04:00 +0800

    feat: add Mutating data demo

---
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -0,0 +1,30 @@
+/*
+ * @FilePath: /Users/i104/next-demo/app/dashboard/invoices/[id]/edit/page.tsx
+ * @author: dongyang(yang.dong@derbysoft.net)
+ */
+import Form from '@/app/ui/invoices/edit-form';
+import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
+import { fetchInvoiceById, fetchCustomers } from '@/app/lib/data';
+
+export default async function Page({ params }: { params: { id: string } }) {
+  const id = params.id;
+  const [invoice, customers] = await Promise.all([
+    fetchInvoiceById(id),
+    fetchCustomers(),
+  ]);
+  return (
+    <main>
+      <Breadcrumbs
+        breadcrumbs={[
+          { label: 'Invoices', href: '/dashboard/invoices' },
+          {
+            label: 'Edit Invoice',
+            href: `/dashboard/invoices/${id}/edit`,
+            active: true,
+          },
+        ]}
+      />
+      <Form invoice={invoice} customers={customers} />
+    </main>
+  );
+}
---
+++ app/dashboard/invoices/create/page.tsx
@@ -0,0 +1,27 @@
+/*
+ * @FilePath: /Users/i104/next-demo/app/dashboard/invoices/create/page.tsx
+ * @author: dongyang(yang.dong@derbysoft.net)
+ */
+import Form from '@/app/ui/invoices/create-form';
+import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
+import { fetchCustomers } from '@/app/lib/data';
+
+export default async function Page() {
+  const customers = await fetchCustomers();
+
+  return (
+    <main>
+      <Breadcrumbs
+        breadcrumbs={[
+          { label: 'Invoices', href: '/dashboard/invoices' },
+          {
+            label: 'Create Invoice',
+            href: '/dashboard/invoices/create',
+            active: true,
+          },
+        ]}
+      />
+      <Form customers={customers} />
+    </main>
+  );
+}
